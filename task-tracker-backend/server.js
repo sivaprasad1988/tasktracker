9dc66bf0b8344e13ff6f024ca51a03eeb95f534b
Siva <siva@webofficeit.com>
2019-02-01 21:20:01 +0530

    Initial commit

---
+++ task-tracker-backend/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require("passport");
+const users = require("./routes/api/users");
+const app = express();
+app.use(
+    bodyParser.urlencoded({
+        urlencoded : false
+    })
+);
+app.use(bodyParser.json());
+const db = require('./config/keys').mongoURI;
+mongoose.connect(db,{ useNewUrlParser: true }).
+            then(() =>{ console.log("MongoDB successfully connected") }).
+            catch((err) => {console.log(err) });
+// Passport middleware
+app.use(passport.initialize());
+require("./config/passport")(passport);
+app.use("/api/users", users);
+
+const port = process.env.PORT || 1000; // process.env.port is Heroku's port if you choose to deploy the app there
+app.listen(port, () => console.log(`Server up and running on port ${port} !`));
